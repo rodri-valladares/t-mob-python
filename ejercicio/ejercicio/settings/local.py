d2a72eec26b14832d25aea1bb7e6db3dcb60aa4c
rodriV <utndavid@gmail.com>
2022-11-30 09:50:24 -0300

    Se configura para funcionar con mysql

--- ejercicio/ejercicio/settings/local.py
+++ ejercicio/ejercicio/settings/local.py
@@ -6,8 +6,12 @@ ALLOWED_HOSTS = []
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test_local',
+        'USER': 'root',
+        'PASSWORD': '12345678',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
