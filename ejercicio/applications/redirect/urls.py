2e2e49cc050ee5ab38570e8ad3e61cbda2bc0f8c
rodriV <utndavid@gmail.com>
2022-11-30 04:06:36 -0300

    Permite filtrar por key

--- ejercicio/applications/redirect/urls.py
+++ ejercicio/applications/redirect/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path('buscar-key/', views.ListRedirectByKey.as_view()),
-    path('buscar-key/', views.RedirectListApiView.as_view()),
+    path('key-list/', views.RedirectListApiView.as_view()),
+    path('key-list/<str:key>/', views.ListRedirectByKey.as_view()),
 ]
--- ejercicio/applications/redirect/views.py
+++ ejercicio/applications/redirect/views.py
@@ -10,21 +10,17 @@ from .serializers import RedirectSerializer
 
 from .models import Redirect
 
-# class ListRedirectByKey(ListView):
-#     template_name = 'home.html'
-#     context_object_name = 'redirect_date'
-
-#     def get_queryset(self):
-#         key_date = self.request.GET.get("kword", '')
-#         lista = Redirect.objects.filter(key=key_date)
-#         return lista
-
 class RedirectListApiView(ListAPIView):
 
     serializer_class = RedirectSerializer
 
     def get_queryset(self):
-        # key_date = self.request.GET.get("kword", '')
-        # lista = Redirect.objects.filter(key=key_date)
-        # return lista
         return Redirect.objects.all()
+
+class ListRedirectByKey(ListAPIView):
+    
+    serializer_class = RedirectSerializer
+
+    def get_queryset(self):
+        key_date = self.kwargs['key']
+        return Redirect.objects.filter(key=key_date)
